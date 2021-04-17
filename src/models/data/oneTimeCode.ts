3c777ecda7d42d79f785e1840079bdd57e35de84
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2021-04-17 12:50:14 -0500

    Updated one time code

--- src/models/data/oneTimeCode.ts
+++ src/models/data/oneTimeCode.ts
@@ -1,7 +1,7 @@
 import { ObjectId } from "mongodb";
 import { IEntity } from "./ientity";
 
-export type OneTimeCodeType = "mfaVerification" | "passwordReset";
+export type OneTimeCodeType = "userVerification" | "newUserVerification" | "passwordReset";
 
 export interface OneTimeCode extends IEntity {
    userId: ObjectId;
