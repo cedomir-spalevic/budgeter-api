61663b3bad7337c832674faf0cbdfa5dbe1a7926
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2021-04-17 13:12:35 -0500

    Abstracted sms messages

--- src/services/internal/verification/phoneNumber.ts
+++ src/services/internal/verification/phoneNumber.ts
@@ -5,6 +5,7 @@ import { IVerification } from "./iVerification";
 import BudgeterMongoClient from "services/external/mongodb/client";
 import { sendTextMessage } from "services/external/aws/sns";
 import { OneTimeCodeType } from "models/data/oneTimeCode";
+import { getSmsMessage } from "views/sms";
 
 class PhoneNumberVerification implements IVerification {
    async sendVerification(userId: ObjectId, phoneNumber: string, type: OneTimeCodeType): Promise<ConfirmationResponse> {
@@ -15,8 +16,8 @@ class PhoneNumberVerification implements IVerification {
       const code = oneTimeCode.code.code.toString();
       await oneTimeCodeService.create(oneTimeCode.code);
       
-      const message = `${code} is your Budgeter confirmation code`;
-      await sendTextMessage(phoneNumber, message);
+      const smsMessage = getSmsMessage(type, code);
+      await sendTextMessage(phoneNumber, smsMessage);
 
       return {
          key: oneTimeCode.code.key,
