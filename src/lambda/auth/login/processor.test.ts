b657d04e8b582c56d1da542865d3ac4e69cac0fb
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2021-04-21 21:04:51 -0500

    lint,format

--- src/lambda/auth/login/processor.test.ts
+++ src/lambda/auth/login/processor.test.ts
@@ -3,9 +3,10 @@ import { processLogin } from "./processor";
 import { test, expect } from "@jest/globals";
 
 test("just testing", () => {
-   return processLogin({ phoneNumber: "6309152350", password: "" })
-            .catch(data => {
-               expect(data).toBeInstanceOf(NoUserEmailFoundError);
-               expect(data.statusCode).toBe(404);
-            })
-})
+   return processLogin({ phoneNumber: "6309152350", password: "" }).catch(
+      (data) => {
+         expect(data).toBeInstanceOf(NoUserEmailFoundError);
+         expect(data.statusCode).toBe(404);
+      }
+   );
+});
